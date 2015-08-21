4ff9ac79f61eb5765a95cdf88d16eea1601708bc
Alex Tkachenko <preusx.dev@gmail.com>
2015-08-21 08:08:42 +0300

    AtRuleTypes bug fixes.

--- atom/js-plugins/AtRuleTypes.js
+++ atom/js-plugins/AtRuleTypes.js
@@ -2,19 +2,19 @@ var AtRuleTypes = function AtRuleTypes() {
   this.type = {};
 };
 
-AtRuleTypes.prototype.exsists = function(name) {
-  return typeof this.type[type] === 'undefined'
+AtRuleTypes.prototype.exists = function(name) {
+  return typeof this.type[name] !== 'undefined';
 };
 
 AtRuleTypes.prototype.register = function(type, callback) {
-  if(typeof type === 'string' || type instanceof String) {
+  if(!(typeof type == 'string' || type instanceof String)) {
     throw new Error('Name of the AtRule must be string instead of ' + typeof type);
 
     return false;
   }
 
   if(this.exists(type)) {
-    throw new Error('AtRule with the name of @' + type + ' already exists.');
+    // throw new Error('AtRule with the name of @' + type + ' already exists.');
 
     return false;
   }
@@ -24,7 +24,7 @@ AtRuleTypes.prototype.register = function(type, callback) {
 
 AtRuleTypes.prototype.evaluate = function(type, evaluator, atrule) {
   if(typeof type === 'string' || type instanceof String) {
-    if(typeof this.type[type] === 'undefined') {
+    if(this.exists(type)) {
       return this.type[type].call(evaluator, atrule);
     }
   }
